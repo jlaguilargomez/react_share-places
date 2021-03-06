9ff0b125e487772f32f61e414adeb5ec212e66bf
jlaguilargomez <jlaguigom@gmail.com>
2021-03-06 16:40:36 +0100

    Basic validatorion for input element

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,10 +1,17 @@
 import Input from '../../shared/components/Input/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/utils/validator';
 import './NewPlace.css';
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title"></Input>
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Enter a valid title"
+      ></Input>
     </form>
   );
 };

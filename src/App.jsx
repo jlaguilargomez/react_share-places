4dbff7404b08bcf97f7ce8e09296fa1f3eecb954
jlaguilargomez <jlaguigom@gmail.com>
2021-03-12 08:33:42 +0100

    Use context to handle authenticate state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useCallback } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -14,24 +15,41 @@ import UserPlaces from './places/pages/UserPlaces/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace/UpdatePlace';
 import Authenticate from './places/pages/Authenticate/Authenticate';
 
-const App = () => {
-  return (
-    <Router>
-      <Navigation />
-      <main>
-        {/* El Switch nos permite que sólo se evalúe una de las rutas y que haga un BREAK después de encontrar la correcta */}
-        <Switch>
-          <Route path="/" exact component={Users}></Route>
-          <Route path="/places/new" exact component={NewPlace}></Route>
-          <Route path="/:userId/places" exact component={UserPlaces}></Route>
-          <Route path="/places/:placeId" exact component={UpdatePlace}></Route>
-          <Route path="/auth" exact component={Authenticate}></Route>
+import { AuthContext } from './shared/context/auth-context';
 
-          {/* Redirigimos a la página de origen en caso de que no coincida con ninguna ruta */}
-          <Redirect to="/"></Redirect>
-        </Switch>
-      </main>
-    </Router>
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      <Router>
+        <Navigation />
+        <main>
+          {/* El Switch nos permite que sólo se evalúe una de las rutas y que haga un BREAK después de encontrar la correcta */}
+          <Switch>
+            <Route path="/" exact component={Users}></Route>
+            <Route path="/places/new" exact component={NewPlace}></Route>
+            <Route path="/:userId/places" exact component={UserPlaces}></Route>
+            <Route
+              path="/places/:placeId"
+              exact
+              component={UpdatePlace}
+            ></Route>
+            <Route path="/auth" exact component={Authenticate}></Route>
+
+            {/* Redirigimos a la página de origen en caso de que no coincida con ninguna ruta */}
+            <Redirect to="/"></Redirect>
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
